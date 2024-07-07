bf9dd87b702675eedd5b0c56f74b55a040ee409d
Yuliya <y.magerramova@itransition.com>
2024-07-07 18:31:13 +0200

    Update importFileParse lambda to send SQS

--- import-service/lambda-functions/importFileParser.ts
+++ import-service/lambda-functions/importFileParser.ts
@@ -5,6 +5,7 @@ import {
   DeleteObjectCommand,
   CopyObjectCommand,
 } from "@aws-sdk/client-s3";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { Readable } from "stream";
 import * as csvParser from "csv-parser";
 
@@ -16,8 +17,10 @@ const headers = {
 };
 
 const BUCKET_NAME = process.env.BUCKET_NAME;
+const SQS_URL = process.env.SQS_URL;
 
-const client = new S3Client();
+const s3Client = new S3Client();
+const sqsClient = new SQSClient({ region: process.env.CDK_REGION });
 
 interface Product {
   title: string;
@@ -35,7 +38,7 @@ export const handler = async (
     const key = record.s3.object.key;
 
     try {
-      const result = await client.send(
+      const result = await s3Client.send(
         new GetObjectCommand({ Bucket: BUCKET_NAME, Key: key })
       );
 
@@ -49,23 +52,32 @@ export const handler = async (
 
       const readableStream = result.Body as Readable;
 
-      const csvData = await new Promise((resolve, reject): void => {
+      await new Promise((resolve, reject): void => {
         const products: Product[] = [];
         readableStream
           .pipe(csvParser({ separator: ";" }))
-          .on("data", (data) => {
+          .on("data", async (data) => {
             products.push(data);
-            console.log(data);
+
+            try {
+              const response = await sqsClient.send(
+                new SendMessageCommand({
+                  QueueUrl: SQS_URL,
+                  MessageBody: JSON.stringify(data),
+                })
+              );
+              console.log("SQS message sent", response.MessageId);
+            } catch (error) {
+              console.error("Error sending SQS message", error);
+            }
           })
           .on("end", () => resolve(products))
           .on("error", reject);
       });
 
-      console.log("csvData"), csvData;
-
       const newKey = key.replace("uploaded", "parsed");
 
-      await client.send(
+      await s3Client.send(
         new CopyObjectCommand({
           Bucket: BUCKET_NAME,
           CopySource: `${BUCKET_NAME}/${key}`,
@@ -75,7 +87,7 @@ export const handler = async (
 
       console.log("A csv file has been copied to the folder parsed");
 
-      await client.send(
+      await s3Client.send(
         new DeleteObjectCommand({
           Bucket: BUCKET_NAME,
           Key: key,
